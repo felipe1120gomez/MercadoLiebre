525a87bb5c9c6d04d814debfd1a384dc10bb9a02
felipe1120gomez <felipe92agudelo@gmail.com>
2022-01-27 08:42:44 -0500

    change path

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-app.use(express.static('public'));
+app.use(express.static('./public'));
 
 
 app.listen(process.env.PORT || 3000, ()=>{
@@ -8,13 +8,13 @@ app.listen(process.env.PORT || 3000, ()=>{
 });
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + './views/index.html');
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(__dirname + './views/login.html');
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(__dirname + './views/register.html');
 });
